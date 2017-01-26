e212f5ab145f2f17887337ad32c4814769dfb4d7
racquemis <racquemis@users.noreply.github.com>
2017-01-26 09:52:06 +0100

    Update CC1101Packet.h

--- Master/Itho/CC1101Packet.h
+++ Master/Itho/CC1101Packet.h
@@ -2,38 +2,24 @@
  * Author: Klusjesman, modified bij supersjimmie for Arduino/ESP8266
  */
 
-#ifndef ITHOPACKET_H_
-#define ITHOPACKET_H_
+#ifndef CC1101PACKET_H_
+#define CC1101PACKET_H_
+
+#include <stdio.h>
+#ifdef ESP8266
+#include <arduino.h>
+#endif
+
+#define CC1101_BUFFER_LEN        64
+#define CC1101_DATA_LEN          CC1101_BUFFER_LEN - 3
 
 
-//do not change enum because they are used in calculations!
-typedef enum IthoCommand 
-{
-	IthoUnknown = 0,
-		
-	IthoJoin = 4,
-	IthoLeave = 8,
-				
-	IthoLow = 35,	
-	IthoMedium = 36,	
-	IthoFull = 37,
-	
-	IthoTimer1 = 41,
-	IthoTimer2 = 51,
-	IthoTimer3 = 61,
-	IthoAway = 34
-};
-
-
-class IthoPacket
+class CC1101Packet
 {
 	public:
-		uint8_t deviceId[8];
-		IthoCommand command;
-		IthoCommand previous;
-		
-		uint8_t counter;		//0-255, counter is increased on every remote button press
+		uint8_t length;
+		uint8_t data[72];
 };
 
 
-#endif /* ITHOPACKET_H_ */
+#endif /* CC1101PACKET_H_ */
