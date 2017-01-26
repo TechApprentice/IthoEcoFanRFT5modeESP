5573c141bdaa8fc25f81a3ed5efbcedd4bab3f6f
racquemis <racquemis@users.noreply.github.com>
2017-01-26 09:47:40 +0100

    Update IthoCC1101.h

--- Master/Itho/IthoCC1101.h
+++ Master/Itho/IthoCC1101.h
@@ -15,12 +15,13 @@ const uint8_t ithoPaTableSend[8] = {0x6F, 0x26, 0x2E, 0x8C, 0x87, 0xCD, 0xC7, 0x
 const uint8_t ithoPaTableReceive[8] = {0x6F, 0x26, 0x2E, 0x7F, 0x8A, 0x84, 0xCA, 0xC4};
 
 //message 1 commands
-const uint8_t ithoMessage1FullCommandBytes[] = {1,84,213,85,50,203,52};
-const uint8_t ithoMessage1MediumCommandBytes[] = {1,84,213,85,74,213,52};
-const uint8_t ithoMessage1LowCommandBytes[] = {1,84,213,85,83,83,84};	
+const uint8_t ithoMessage1FullCommandBytes[] = {1,53,84,204,205,84,170};//{1,84,213,85,50,203,52};
+const uint8_t ithoMessage1MediumCommandBytes[] = {0,213,85,74,171,84,170};//{1,84,213,85,74,213,52};
+const uint8_t ithoMessage1LowCommandBytes[] = {0,213,85,83,45,52,170};//{1,84,213,85,83,83,84};	
 const uint8_t ithoMessage1Timer1CommandBytes[] = {1,83,83,84,204,202,180};	
 const uint8_t ithoMessage1Timer2CommandBytes[] = {1,83,83,83,53,52,180};		
-const uint8_t ithoMessage1Timer3CommandBytes[] = {1,83,83,82,173,82,180};			
+const uint8_t ithoMessage1Timer3CommandBytes[] = {1,83,83,82,173,82,180};	
+const uint8_t ithoMessage1AwayCommandBytes[] = {1,53,84,204,205,84,170};		
 const uint8_t ithoMessage1JoinCommandBytes[] = {0,170,171,85,84,202,180};	
 const uint8_t ithoMessage1LeaveCommandBytes[] = {0,170,173,85,83,43,84};	
 
@@ -31,6 +32,7 @@ const uint8_t ithoMessage2LowCommandBytes[] = {6,89,150,170,165,101,90,150,85,14
 const uint8_t ithoMessage2Timer1CommandBytes[] = {6,89,150,170,169,101,90,150,85,149,101,89,86,85,153};
 const uint8_t ithoMessage2Timer2CommandBytes[] = {6,89,150,170,169,101,90,150,85,149,101,89,86,149,150};
 const uint8_t ithoMessage2Timer3CommandBytes[] = {6,89,150,170,169,101,90,150,85,149,101,89,86,149,154};
+const uint8_t ithoMessage2AwayCommandBytes[] = {6,89,150,170,165,101,90,150,85,149,101,90,86,85,150};
 const uint8_t ithoMessage2JoinCommandBytes[] = {9,90,170,90,165,165,89,106,85,149,102,89,150,170,165};
 const uint8_t ithoMessage2LeaveCommandBytes[] = {9,90,170,90,165,165,89,166,85,149,105,90,170,90,165};
 
