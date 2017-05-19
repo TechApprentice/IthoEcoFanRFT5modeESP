6263f61f5b00af1bfccc7bd9719efb32c7761f56
TechApprentice <evanderwesten@gmail.com>
2017-05-19 11:16:32 +0200

    Update IthoEcoFanRFT.ino
    Changes names of the modes to my personal preferences. Modes are: Away, low, medium, high, fullpower and timer.

--- Master/IthoEcoFanRFT/IthoEcoFanRFT.ino
+++ Master/IthoEcoFanRFT/IthoEcoFanRFT.ino
@@ -1,130 +1,188 @@
-/*
- * Original Author: Klusjesman
- *
- * Tested with STK500 + ATMega328P
- * GCC-AVR compiler
- * 
- * Modified by supersjimmie:
- * Code and libraries made compatible with Arduino and ESP8266 
- * Tested with Arduino IDE v1.6.5 and 1.6.9
- * For ESP8266 tested with ESP8266 core for Arduino v 2.1.0 and 2.2.0 Stable
- * (See https://github.com/esp8266/Arduino/ )
- * 
- */
-
-/*
-CC11xx pins    ESP pins Arduino pins  Description
-1 - VCC        VCC      VCC           3v3
-2 - GND        GND      GND           Ground
-
-3 - MOSI       13=D7    Pin 11        Data input to CC11xx
-4 - SCK        14=D5    Pin 13        Clock pin
-5 - MISO/GDO1  12=D6    Pin 12        Data output from CC11xx / serial clock from CC11xx
-6 - GDO0       ?        Pin 2?        Serial data to CC11xx
-7 - GDO2       ?        Pin  ?        output as a symbol of receiving or sending data
-8 - CSN        15=D8    Pin 10        Chip select / (SPI_SS)
+/* 
+*    Created trough the colaboration of Tech enthousiasts.
+*    More info at: https://gathering.tweakers.net/forum/list_messages/1690945
+*    Thanks to all involved for making this work
 */
 
 #include <SPI.h>
 #include "IthoCC1101.h"
 #include "IthoPacket.h"
+#include <ESP8266WiFi.h>
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+ const char WiFiSSID[] = "Your-SSID";
+ const char WiFiPSK[] = "Your-PSK";
+// Commands to send (trough webbrowser or Domoticz)
+// <ip-adress-nodemcu>/away
+// <ip-adress-nodemcu>/low
+// <ip-adress-nodemcu>/medium
+// <ip-adress-nodemcu>/high
+// <ip-adress-nodemcu>/power
+// <ip-adress-nodemcu>/timer
+
+
 
 IthoCC1101 rf;
 IthoPacket packet;
+WiFiServer server(80);
 
-void setup(void) {
+void setup() 
+{
+
   Serial.begin(115200);
   delay(500);
-  Serial.println("setup begin");
+  Serial.println("");
+  Serial.println("ESP8266 remote for Itho ventilation unit");
+  Serial.println("See https://gathering.tweakers.net/forum/list_messages/1690945 for more information!");
+  Serial.println("Setting up CC1101 module...");
   rf.init();
-  Serial.println("setup done");
+  Serial.println("CC1101 transmitter is now ready!");
+  Serial.println("");
   sendRegister();
-  Serial.println("join command sent");
+  connectWiFi();
+  Serial.println("");
+  // Start the server
+  Serial.println("Starting webserver...");
+  server.begin();
+  Serial.println("Webserver is now running!");
+  
 }
 
-void loop(void) {
-	//set CC1101 registers
-	rf.initReceive();
-	Serial.print("start\n");
-	sei();
+void loop() 
+{
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
 
-	while (1==1) {
-		if (rf.checkForNewPacket()) {
-			packet = rf.getLastPacket();
-			//show counter
-			Serial.print("counter=");
-			Serial.print(packet.counter);
-			Serial.print(", ");
-			//show command
-			switch (packet.command) {
-        case IthoUnknown:
-          Serial.print("unknown\n");
-          break;
-        case IthoAway:
-          Serial.print("away\n");
-          break;
-        case IthoLow:
-          Serial.print("low\n");
-          break;
-        case IthoMedium:
-          Serial.print("medium\n");
-          break;
-        case IthoFull:
-          Serial.print("full\n");
-          break;
-        case IthoTimer1:
-          Serial.print("timer1\n");
-          break;
-        case IthoTimer2:
-          Serial.print("timer2\n");
-          break;
-        case IthoTimer3:
-          Serial.print("timer3\n");
-          break;
-        case IthoJoin:
-          Serial.print("join\n");
-          break;
-        case IthoLeave:
-          Serial.print("leave\n");
-          break;
-			} // switch (recv) command
-		} // checkfornewpacket
-	yield();
-	} // while 1==1
-} // outer loop
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+  const char* val;
+
+  if (req.indexOf("/away") != -1) {
+    val = "away";
+    sendAway();
+  }
+  else if (req.indexOf("/low") != -1) {
+    val = "low";
+    sendLowSpeed();
+  }
+  else if (req.indexOf("/medium") != -1) {
+    val = "medium";
+    sendMediumSpeed();
+  }
+  else if (req.indexOf("/high") != -1) {
+    val = "high";
+    sendHighSpeed();    
+  }
+  else if (req.indexOf("/power") != -1) {
+    val = "power";
+    sendFullPower();
+  }
+  else if (req.indexOf("/timer") != -1) {
+  val = "timer";
+    sendTimer();    
+  }
+  client.flush();
+
+  // Prepare the response. Start with the common header:
+  String s = "HTTP/1.1 200 OK\r\n";
+  s += "Content-Type: text/html\r\n\r\n";
+  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  // If we're setting the LED, print out a message saying we did
+  if (val != NULL)
+  {
+    s += "Fan is now: ";
+    s += val;
+  }
+  else
+  {
+    s += "Invalid Request.<br> Try /standby, /low, /medium, /high, /power or /timer.";
+  }
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disconnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+}
+
+void connectWiFi()
+{
+  //byte ledStatus = LOW;
+
+  // Set WiFi mode to station (as opposed to AP or AP_STA)
+  WiFi.mode(WIFI_STA);
+  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
+  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
+  // or WEP passphrase.
+  Serial.println();
+  Serial.print("Connecting to wifi-network: ");
+  Serial.print(WiFiSSID);
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  // Use the WiFi.status() function to check if the ESP8266
+  // is connected to a WiFi network.
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("I am now connected to '");
+  Serial.print(WiFiSSID);
+  Serial.print("' and my IP-address is: ");
+  Serial.println(WiFi.localIP());
+}
 
 void sendRegister() {
-   Serial.println("sending join...");
+  Serial.println("Sending command 'join' to Itho ventilation unit...");
    rf.sendCommand(IthoJoin);
-   Serial.println("sending join done.");
+   Serial.println("Command 'join' has been sent!");
 }
 
 void sendAway() {
-   Serial.println("sending away...");
+   Serial.println("Sending command 'away' to Itho ventilation unit...");
    rf.sendCommand(IthoAway);
-   Serial.println("sending away done.");
+   Serial.println("Command 'away' has been sent!");
 }
+
 void sendLowSpeed() {
-   Serial.println("sending low...");
+   Serial.println("Sending command 'low' to Itho ventilation unit...");
    rf.sendCommand(IthoLow);
-   Serial.println("sending low done.");
+   Serial.println("Command 'low' has been sent!");
 }
 
 void sendMediumSpeed() {
-   Serial.println("sending medium...");
+   Serial.println("Sending command 'medium' to Itho ventilation unit...");
    rf.sendCommand(IthoMedium);
-   Serial.println("sending medium done.");
+   Serial.println("Command 'medium' has been sent!");
 }
 
-void sendFullSpeed() {
-   Serial.println("sending FullSpeed...");
-   rf.sendCommand(IthoFull);
-   Serial.println("sending FullSpeed done.");
+void sendHighSpeed() {
+   Serial.println("Sending command 'fullspeed' to Itho ventilation unit...");
+   rf.sendCommand(IthoHigh);
+   Serial.println("Command 'fullspeed' has been sent!");
+}
+
+void sendFullPower() {
+   Serial.println("Sending command 'power' to Itho ventilation unit...");
+   rf.sendCommand(IthoFullPower);
+   Serial.println("Command 'power' has been sent!");
 }
 
 void sendTimer() {
-   Serial.println("sending timer...");
+   Serial.println("Sending command 'timer' to Itho ventilation unit...");
    rf.sendCommand(IthoTimer1);
-   Serial.println("sending timer done.");
+   Serial.println("Command 'timer' has been sent!");
 }
-
